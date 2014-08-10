4ab4e866345d3af95bb70db79dce67455622976a
Takashi Ando <a-t.t_y1040sm@ezweb.ne.jp>
2014-08-10 15:14:45 +0900

    黒い丸

---
+++ src/com/example/drawapp/SampleView.java
@@ -0,0 +1,33 @@
+package com.example.drawapp;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+
+public class SampleView extends View {
+	
+	private Paint paint=new Paint();
+	
+	public SampleView(Context context){
+		super(context);
+		setBackgroundColor(Color.WHITE);
+		
+	}
+	
+	@Override
+public void onDraw(Canvas canvas){
+		canvas.drawCircle(100,100,5,paint);
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
